88e1f02a40156c183e2cd118b8ab8c4fc68efbfd
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-03-28 05:41:41 +0000

    msm8974-common: Drop support for building twrp
    * No this doesn't break twrp support in any way, twrp will still work.
      All this does is remove the makefiles to build twrp using this device
      tree which we don't currently do anyway, so removing for now.
    
    Change-Id: Icd4403e5b73c5aec977a6d5afa31720703eb9b39
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,14 +126,6 @@ include device/qcom/sepolicy-legacy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
-# TWRP
-ifeq ($(WITH_TWRP),true)
-TARGET_RECOVERY_DEVICE_DIRS += $(PLATFORM_PATH)/twrp
-TW_INCLUDE_CRYPTO := true
-TW_CRYPTO_USE_SBIN_VOLD := true
-TW_THEME := portrait_hdpi
-endif
-
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
 BOARD_WLAN_DEVICE                := qcwcn
